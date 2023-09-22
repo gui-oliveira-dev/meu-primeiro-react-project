ffaffc05d09725841363d01b9870e0d45d3eb8b3
Guilherme Oliveira <guigooliveraa@gmail.com>
2023-09-22 20:22:12 -0300

    muda fivecons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,11 @@
 import './style.css'
 
-const name = 'Guilherme'
-
 function Header () {
   return (
   <header className="header">
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
     <h2>
-      {`Seja bem Vindo ${name}!`}
+      {`Seja bem Vindo!`}
     </h2>
   </header>
   )
